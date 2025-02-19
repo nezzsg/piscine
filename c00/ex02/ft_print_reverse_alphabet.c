123435b6519784ec2d2183ad9f45c3ad66eb905f
Savignaud juliette <nezz@nezzs-MacBook-Air.local>
2025-02-19 23:14:33 +0100

    :poop: caca prout prout

---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jusavign <jusavign@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/19 22:42:05 by jusavign          #+#    #+#             */
+/*   Updated: 2025/02/19 22:46:19 by jusavign         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	write(1, "zyxwvutsrqponmlkjihgfedcba", 26)
+}
